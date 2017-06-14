65874e2a62a1fa3558c3c3e8b30a8e234c49d07d
Bas <basvanderl@hotmail.com>
2017-06-14 13:49:39 +0200

    no

---
+++ Top-2001/Top2000/ClassLibrary/Artist.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary
+{
+    public class Artist
+    {
+        private string name;
+        private string biography;
+        private string url;
+        private string picture;
+
+        public Artist(string name, string biography = null, string url = null, string picture = null)
+        {
+            this.name = name;
+            this.biography = biography;
+            this.url = url;
+            this.picture = picture;
+        }
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+
+            set
+            {
+                name = value;
+            }
+        }
+
+        public string Biography
+        {
+            get
+            {
+                return biography;
+            }
+
+            set
+            {
+                biography = value;
+            }
+        }
+
+        public string Url
+        {
+            get
+            {
+                return url;
+            }
+
+            set
+            {
+                url = value;
+            }
+        }
+
+        public string Picture
+        {
+            get
+            {
+                return picture;
+            }
+
+            set
+            {
+                picture = value;
+            }
+        }
+    }
+}
--- Top-2001/Top2000/ClassLibrary/Record.cs
+++ Top-2001/Top2000/ClassLibrary/Record.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace ClassLibrary
 {
-    class Record
+    public class Record
     {
         private int position;
         private string title;
